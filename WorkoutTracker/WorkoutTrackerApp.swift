c312385ba0faa8523d6bc241cc07048430b570a9
boris <barys.serzhanovich@gmail.com>
2025-12-24 13:51:09 +0300

    Initial Commit

---
+++ WorkoutTracker/WorkoutTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTrackerApp.swift
+//  WorkoutTracker
+//
+//  Created by Boris Serzhanovich on 24.12.25.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
